97b506358575030dec07f74c9c5044073e4b8dc7
d3-jon-allen <112494862+d3-jon-allen@users.noreply.github.com>
2025-10-22 16:43:28 -0400

    remove unused custom cards

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,5 @@
 export * from './local';
 
 // Export custom components
-export { default as CustomCardNew } from './CustomCardNew';
-export { default as CustomCardNewFlat } from './CustomCardNewFlat';
 export { default as SidebarNavigation } from './SidebarNavigation';
-export { default as SimpleCard } from './SimpleCard';
 export { default as LessSimpleCard } from './LessSimpleCard';
